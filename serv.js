e5c709103b69f8c19960b03fabab1a2e763aa7d1
INF-Zenyth <samuel.ward@outlook.fr>
2022-03-29 12:45:17 +0200

    0.0.4
    Co-authored-by: lefranccharles11@outlook.fr <lefranccharles11@outlook.fr>
    Co-authored-by: RENAULT KYLIAN <kylian.renault@viacesi.fr>

--- serv.js
+++ serv.js
@@ -1,35 +1,41 @@
 import connection from "./sql_connection/connection.js";
+import morgan from "morgan";
 import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 const app = express();
 const port = 3002;
 
-app.use(bodyParser.json());
+app.use(cors({ credentials: true }));
+app.use(morgan('tiny'));
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+ 
 
 connection.connect(err => {
     if(err) throw err;
-    console.log("Connection with MySQL established...");
+    console.log("Connection with MySQL established.");
 });
 
 app.get('/api/:table/:id', (req,res) => { //request used to select one element in a specified table
     const { table, id } = req.params;
-    const selectOneQuery = `SELECT * FROM ${table} WHERE id=${id}`;
-    connection.query(selectOneQuery, (err, result) => {
+    console.log(table); 
+    const selectOneQuery = `SELECT * FROM ${table} WHERE id=?`;
+    connection.query(selectOneQuery, id , (err, result) => {
         if(err) throw err;
         res.send(result);
-    });
+    }); 
 });
 
 app.get('/api/:table', (req,res) => { //request used to select all elements in a specified table
     const table = req.params.table;
-    const selectAllQuery = `SELECT * FROM ${table}`;
+    const selectAllQuery = `SELECT * FROM ${table};`;
+    console.log(selectAllQuery)
     connection.query(selectAllQuery, (err, result) => {
         if(err) throw err;
-        res.send(result);
+        res.send(result); 
     });
 });
 
@@ -51,7 +57,7 @@ app.post('/api/:table', (req,res) => { //insert values from http body into a spe
         insertQuery = `INSERT INTO ${table} (${result.join(',')}) VALUES (${"?, ".repeat(data.length).slice(0,-2)})`;
         console.log(insertQuery);
         connection.query(insertQuery, data, (err, result) => {
-            if(err) res.send("Wrong data entered");
+            if(err) res.send("Wrong data entered"); 
             res.send(result);
         });
     });
@@ -65,13 +71,13 @@ app.put('/api/:table/:id', (req, res) => { //update
         message: "Bad body, try again"
     });
     let updateQuery;
-    const columnsQuery = `DESCRIBE ${ table }`;
+    const columnsQuery = `DESCRIBE ${table}`;
     connection.query(columnsQuery, (err, result) => {
         if(err) throw err;
         result.shift();
         result = result.map(val => val.Field);
         data = Object.values(data);
-        if(data.length !== result.length) res.send("wrong data size");
+        if(data.length !== result.length) res.send("wrong data size"); 
         data.push(id);
         let tempString = ""
         for(let i = 0; i < data.length-1; i++) {
@@ -88,8 +94,8 @@ app.put('/api/:table/:id', (req, res) => { //update
 
 app.delete('/api/:table/:id', (req,res) => {
     const { table, id } = req.params;
-    const deleteQuery = `DELETE FROM ${table} WHERE id=${id}`;
-    connection.query(deleteQuery, (err, result) => {
+    const deleteQuery = `DELETE FROM ? WHERE id=?`;
+    connection.query(deleteQuery, [table, id], (err, result) => {
         if(err) throw err;
         res.send(result);
     });
--- sql_connection/connection.js
+++ sql_connection/connection.js
@@ -5,7 +5,7 @@ export default mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "web"
+    database: "lahya"
 });
 
 
